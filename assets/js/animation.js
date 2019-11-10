1a66c11b4f52beac5d8e11d4dbd3e13285eb3aea
Aderinsola Akintilo <aderinsola419@gmail.com>
2019-11-10 20:17:25 +0100

    got the first round of scrollOut & animeJs working. will iterate on it

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,21 +1,47 @@
 // as soon as dom is loaded, execute functions
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    ScrollOut();
+
     // initial page load animation
-    console.log("dom loaded")
-    anime.timeline({
-            easing: 'easeInOutQuad'
-        })
-        .add({
-            targets: '.section-header h1', 
-            opacity: [0, 1], 
-            translateY: [50, 0],
-            delay: 1000, 
-            duration: 1000,
-            offset: '+=50', 
-            scale: [0.75, 1]
-            // width: ['0%', '100%']
-        })
+    console.log("dom loaded");
+
+    anime({
+        easing: 'easeInOutQuad',
+        targets: '.section-header h1',
+        opacity: [0, 1],
+        translateY: [50, 0],
+        delay: 1000,
+        duration: 1000,
+        scale: [0.90, 1]
+    });
+});
+
+
+ScrollOut({
+    onShown: function (el) {
+        // use the web animation API
+        anime({
+            threshold: 0.5,
+            easing: 'easeInOutQuad',
+            targets: el,
+            opacity: [0, 1],
+            duration: 1500,
+            scale: [0.95, 1]
+        });
+    },
+    onHidden: function (el) {
+        // hide the element initially
+        anime({
+            threshold: 0.5,
+            easing: 'easeInOutQuad',
+            targets: el,
+            opacity: [1, 0],
+            duration: 1500,
+            scale: [1, 0.95]
+        });
+    },
 });
 
 // https://www.youtube.com/watch?v=XiC5Lrh6CZY
