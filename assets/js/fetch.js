686f6993f5d989316f591d78b721692f9fa173e3
Aderinsola Akintilo <aderinsola419@gmail.com>
2019-11-29 18:28:44 +0100

    rollback here if stuff starts to break ----XXXXXXXX-----

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -38,96 +38,117 @@ function removeHomePageContainers(document) {
     }
 }
 
-function addProjectPageContainers(document, doc) {
-    setTimeout(() => {
-        // need to scroll to the top
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-
-        // replace the content with new content
-        let projectSections = doc.querySelectorAll('.section');
-        for (var i = 0; i < projectSections.length; i++) {
-            document.querySelector('body').appendChild(projectSections[i]);
-        }
-
-        // animate the replacement
-        anime({
-            easing: 'easeInOutQuad',
-            targets: '.section',
-            // opacity: [0, 1],
-            duration: 700,
-            scale: [0.99, 1]
-        });
-    }, 700);
-
-    let scrollOutItems = document.querySelectorAll('[data-scroll]');
-    let arrs = [...scrollOutItems]; 
+function scrollOutCall() {
+    console.log("6. starting to attach scroll out function")
     ScrollOut({
-        onShown: function () {
+        onShown: function (el) {
             // use the web animation API
-            console.log("visibile ");
+            console.log("on shown ");
             anime({
-                threshold: 0.9,
                 easing: 'easeInOutQuad',
-                targets: scrollOutItems,
+                targets: el, // ".cigna.section.section-overview.section-columns",
                 opacity: [0, 1],
                 duration: 1200,
                 scale: [0.99, 1]
             });
         },
-        onHidden: function () {
+        onHidden: function (el) {
             // hide the element initially
+            console.log("on hide ");
             anime({
-                threshold: 0.5,
                 easing: 'easeInOutQuad',
-                targets: scrollOutItems,
+                targets: el, //".cigna.section.section-overview.section-columns",
                 opacity: [1, 0],
                 duration: 1200,
                 scale: [1, 0.99]
             });
         },
     });
+}
 
-    // attach animation event lisnters
+function addProjectPageContainers(document, doc) {
+    // hide the body 
+    // anime({
+    //     targets: 'body',
+    //     opacity: [1, 0]
+    // })
+    // need to scroll to the top
+    console.log('1. scroll to top')
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 
-    // add new event listeners
-    let homeLink = doc.querySelector('#home-link');
-    let cignaLink = doc.querySelector('#cigna-link');
-    let fedexLink = doc.querySelector('#fedex-link');
-    let gokadaLink = doc.querySelector('#gokada-link');
-    let siriuslabsLink = doc.querySelector('#siriuslabs-link');
-    let arr = [homeLink, cignaLink, fedexLink, gokadaLink, siriuslabsLink];
+    // replace the content with new content
+    console.log('2. attaching elements to dom')
+    let projectSections = doc.querySelectorAll('.section');
+    for (var i = 0; i < projectSections.length; i++) {
+        document.querySelector('body').appendChild(projectSections[i]);
+    }
 
-    arr.forEach((eachLink) => {
-        eachLink.addEventListener('click', (event) => {
-            event.preventDefault();
-            switch (eachLink) {
+    // animate the replacement
+    console.log('3. animating dom elements to 0')
+    anime({
+        easing: 'easeInOutQuad',
+        targets: '.section',
+        opacity: [1, 0],
+        duration: 0,
+        scale: [1, 0.99]
+    });
 
-                case homeLink:
-                    fetchPage(eachLink, 'index.html');
-                    break;
-
-                case cignaLink:
-                    fetchPage(eachLink, 'cigna.html');
-                    break;
-
-                case fedexLink:
-                    fetchPage(eachLink, 'fedex.html');
-                    break;
-
-                case gokadaLink:
-                    fetchPage(eachLink, 'gokada.html');
-                    break;
-
-                case siriuslabsLink:
-                    fetchPage(eachLink, 'siriuslabs.html');
-                    break;
-
+    setTimeout(() => {
+        console.log("4. calling set timeout and animating elements to 1 \n")
+        // animate the replacement
+        anime({
+            easing: 'easeInOutQuad',
+            targets: '.section',
+            opacity: [0, 1],
+            duration: 700,
+            scale: [0.99, 1],
+            complete: function () {
+                console.log("5. finished running anime function");
+                scrollOutCall();
             }
         });
-    })
+    }, 6700);
+
+
+    // // add new event listeners
+    // let homeLink = doc.querySelector('#home-link');
+    // let cignaLink = doc.querySelector('#cigna-link');
+    // let fedexLink = doc.querySelector('#fedex-link');
+    // let gokadaLink = doc.querySelector('#gokada-link');
+    // let siriuslabsLink = doc.querySelector('#siriuslabs-link');
+    // let arr = [homeLink, cignaLink, fedexLink, gokadaLink, siriuslabsLink];
+
+    // arr.forEach((eachLink) => {
+    //     eachLink.addEventListener('click', (event) => {
+    //         event.preventDefault();
+    //         switch (eachLink) {
+
+    //             case homeLink:
+    //                 fetchPage(eachLink, 'index.html');
+    //                 break;
+
+    //             case cignaLink:
+    //                 fetchPage(eachLink, 'cigna.html');
+    //                 break;
+
+    //             case fedexLink:
+    //                 fetchPage(eachLink, 'fedex.html');
+    //                 break;
+
+    //             case gokadaLink:
+    //                 fetchPage(eachLink, 'gokada.html');
+    //                 break;
+
+    //             case siriuslabsLink:
+    //                 fetchPage(eachLink, 'siriuslabs.html');
+    //                 break;
+
+    //         }
+    //     });
+    // })
 }
 
 function fetchPage(link, page) {
