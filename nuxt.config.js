af52b3b362fe4e03a3b01e5007d58a26050281d0
Aderinsola Akintilo <aderinsola419@gmail.com>
2021-02-03 19:46:44 +0100

    got the structure started for 2021

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,26 +1,56 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'portfolio',
-    htmlAttrs: {
-      lang: 'en'
-    },
+    title: 'Aderinsola Akintilo | Product Manager | Product Designer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Aderinsola Akintilo helps companies design and architect software solutions in the healthcare and transportation sectors. ',
+      },
+      {
+        hid: 'ogsitename',
+        property: 'og:site_name',
+        content: 'Aderinsola Akintilo | Product Manager | Product Designer',
+      },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        type: 'image/x-icon',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        sizes: '32x32',
+        type: 'image/x-icon',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        sizes: '16x16',
+        type: 'image/png',
+        href: '/favicon-16x16.png',
+      },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'canonical', href: 'https://www.aderinsola.co/' },
+    ],
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
+  // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
+    // 'bootstrap/dist/css/bootstrap.css',
+    // 'bootstrap-vue/dist/bootstrap-vue.css',
+    // '~/assets/css/main.scss',
   ],
+
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
