4e1e31abbeb9d491c0f663ae8bf334bbebdcca38
Aderinsola Akintilo <aderinsola419@gmail.com>
2021-02-10 21:22:04 +0100

    adding header tags for SEO

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,52 +1,54 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'aderinsola',
+    title: "Portfolio of Aderinsola Akintilo",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      {
+        hid: "ogsitename",
+        property: "og:site_name",
+        content: "Portfolio of Aderinsola Akintilo",
+      },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "canonical", href: "https://aderinsola.co/" },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/css/reset.css',
-    '~/assets/css/main.css',
-    '~/assets/css/media-queries.css',
+    "~/assets/css/reset.css",
+    "~/assets/css/main.css",
+    "~/assets/css/media-queries.css",
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }, 
+  build: {},
 
   generate: {
-    fallback: true
-  }
-}
+    fallback: true,
+  },
+};
