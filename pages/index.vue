34d613dab0e364c377423ab7acc00f0a09cf2498
Aderinsola Akintilo <aderinsola419@gmail.com>
2021-02-13 19:08:04 +0100

    added new portfolio pdf link

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,70 @@
 <template>
   <div class="container" xyz="fade in stagger ease-in-out delay-15">
-      <!-- <Logo /> -->
-      <header class="xyz-in"> 
-        <a class="logo" href="/">Aderinsola Akintilo</a> 
-        <p class="role">Product Designer / Product Manager</p>
-      </header>
-      <section class="content xyz-in">
-        <h1>
-          Designing and managing software solutions in healthcare and transportation sectors. 
-        </h1>
-        <h5>
-          My site is currently under-construction, but reach out if you would like to work together. 
-        </h5>
-        <a href="mailto:aderinsola.akintilo@gmail.com?subject=Let's Chat! – aderinsola.co" target="_blank" class="btn btn-primary">
+    <!-- <Logo /> -->
+    <header class="xyz-in">
+      <a class="logo" href="/">Aderinsola Akintilo</a>
+      <p class="role">Product Designer / Product Manager</p>
+    </header>
+    <section class="content xyz-in">
+      <h1>
+        Designing and managing software solutions in healthcare and
+        transportation sectors.
+      </h1>
+      <h5>
+        My site is currently under-construction, but reach out if you would like
+        to work together.
+      </h5>
+      <a
+        href="mailto:aderinsola.akintilo@gmail.com?subject=Let's Chat! – aderinsola.co"
+        target="_blank"
+        class="btn btn-primary"
+      >
         Contact Me
-        </a>
-      </section>
-      <footer class="xyz-in">
-        <div class="row">
-          <a href="https://drive.google.com/file/d/1bv8yTFtZl-pJIa_DKh99bAVbC_TNbq1D/view?usp=sharing" target="_blank" class="footer-links">Portfolio</a>
-          <a href="https://drive.google.com/file/d/1VePb7GpRjXpibKDbhZX6MVyMYYwTYjeF/view?usp=sharing" target="_blank" class="footer-links center">Resume</a>
-          <a href="https://www.linkedin.com/in/aderinsola/" target="_blank" class="footer-links right">LinkedIn</a>
-        </div>
-        <div class="row">
-          <a href="https://dribbble.com/Aderinsola" target="_blank" class="footer-links">Dribbble</a>
-          <a href="https://medium.com/@__Aderinsola__" target="_blank" class="footer-links center">Blog</a>
-          <a href="mailto:aderinsola.akintilo@gmail.com?subject=We Have Questions! – aderinsola.co" target="_blank" class="footer-links right">Email</a>
-        </div>
-
-      </footer>
-      <!-- <div class="links">
+      </a>
+    </section>
+    <footer class="xyz-in">
+      <div class="row">
+        <a
+          href="https://drive.google.com/file/d/1dp5TUI7-VeFU5bSMfwjO83eHuGX0AnSf/view?usp=sharing"
+          target="_blank"
+          class="footer-links"
+          >Portfolio</a
+        >
+        <a
+          href="https://drive.google.com/file/d/1VePb7GpRjXpibKDbhZX6MVyMYYwTYjeF/view?usp=sharing"
+          target="_blank"
+          class="footer-links center"
+          >Resume</a
+        >
+        <a
+          href="https://www.linkedin.com/in/aderinsola/"
+          target="_blank"
+          class="footer-links right"
+          >LinkedIn</a
+        >
+      </div>
+      <div class="row">
+        <a
+          href="https://dribbble.com/Aderinsola"
+          target="_blank"
+          class="footer-links"
+          >Dribbble</a
+        >
+        <a
+          href="https://medium.com/@__Aderinsola__"
+          target="_blank"
+          class="footer-links center"
+          >Blog</a
+        >
+        <a
+          href="mailto:aderinsola.akintilo@gmail.com?subject=We Have Questions! – aderinsola.co"
+          target="_blank"
+          class="footer-links right"
+          >Email</a
+        >
+      </div>
+    </footer>
+    <!-- <div class="links">
         <a
           href="https://nuxtjs.org/"
           target="_blank"
@@ -51,6 +86,5 @@
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
-
